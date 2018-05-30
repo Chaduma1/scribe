d5645a388d6ce9a72db9a953fa2e1961a575f246
Chad Smith <chasmith@ultimatemedical.edu>
2018-05-29 21:59:11 -0400

    HTML 5
    HTML5

--- mobile/story_content/5xxB5DE1zhl_22050_48_0_captions.mp3.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.15.15581.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.004%0D%0ATim,%20please%20provide%20me%20with%20the%20test%20results.%0D%0A%0D%0A"}')
