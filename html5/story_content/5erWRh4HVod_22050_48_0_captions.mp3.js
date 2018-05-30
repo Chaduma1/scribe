9e8dd5e335334498c1c1d233bc5177e26a89b120
Chad Smith <chasmith@ultimatemedical.edu>
2018-05-29 22:12:21 -0400

    fix game
    fix game

---
+++ html5/story_content/5erWRh4HVod_22050_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.15.15581.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.605%0D%0AAlso,%20please%20research%20common%20cold%20remedies%20for%20Mr.%20Smith.%20%0D%0A%0D%0A"}')
