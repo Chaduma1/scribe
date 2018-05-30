0c0e783e851cdf12adca1de14a5ed3cbfb1b306d
Chaduma1 <39747224+Chaduma1@users.noreply.github.com>
2018-05-29 22:31:08 -0400

    Add files via upload

---
+++ story_content/story_content/5Vl9bbLCXUb_22050_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.15.15581.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.187%0D%0AI%20can%E2%80%99t%20today%20I%20am%20proofreading%20medical%20records.%20%0D%0A%0D%0A"}')
