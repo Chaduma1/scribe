26fbc0a0d2d76e301b88fb59fdad050a2487bd39
Chad Smith <chasmith@ultimatemedical.edu>
2018-06-06 11:12:38 -0400

    4
    4

--- story_content/user.js
+++ story_content/user.js
@@ -2,7 +2,7 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5YZ0RyldvJT":
+      case "5m10W0DmlWV":
         Script1();
         break;
   }
@@ -10,26 +10,6 @@ function ExecuteScript(strId)
 
 function Script1()
 {
-  if (document.location.href.indexOf('html5') < 0) {
-GetPlayer().printSlide()
-} else {
-if(!window.hasPrintStyle){
-window.hasPrintStyle = true;
-var css = '@media print {body * {visibility: hidden;}#slidecontainer, #slidecontainer * {visibility: visible;}#slidecontainer {position: absolute;left: 0;top: 0; }#slideframe {overflow: visible;}}',
-head = document.head || document.getElementsByTagName('head')[0],
-style = document.createElement('style');
-style.type = 'text/css';
-if (style.styleSheet){
-style.styleSheet.cssText = css;
-} else {
-style.appendChild(document.createTextNode(css));
-}
-head.appendChild(style);
-}
-var whereNow = $("#slidecontainer").offset();
-$("#slidecontainer").offset({top:0,left:0});
-window.print();
-$("#slidecontainer").offset(whereNow);
-}
+  window.print()
 }
 
